06bb62a5ba5de6cb47e8428a859380423fbd6d83
Udayanvats <udayan.asus06@gmail.com>
2023-02-11 23:22:01 +0530

    Connections of all registration and Login pages made

--- lib/onboarding_screen/adminOnBoard.dart
+++ lib/onboarding_screen/adminOnBoard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:wastenot/admin/pages/login/auth_page.dart';
+
 class Second_screen extends StatefulWidget {
   const Second_screen({super.key});
 
@@ -13,51 +15,40 @@ class _Second_screenState extends State<Second_screen> {
     return Container(
       color: Colors.transparent,
       child: Column(
-
         children: [
-
-           SizedBox( 
-            height: MediaQuery.of(context).size.height * 0.1,),
-         
-
-           Lottie.asset('assets/adminLottie.json'),
-            SizedBox( 
-            height: MediaQuery.of(context).size.height * 0.17,),
-
-           Text("Admin",
-            style: TextStyle(fontWeight: FontWeight.bold,
-            fontSize: 50
-            
-            ),
-           
-           
-           ),
-            SizedBox( 
-            height: MediaQuery.of(context).size.height * 0.05,),
-
-           ElevatedButton(
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          Lottie.asset('assets/adminLottie.json'),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.17,
+          ),
+          Text(
+            "Admin",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          ElevatedButton(
             style: const ButtonStyle(
               backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
             ),
             child: const Text(
               'Admin',
-              style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
             ),
             onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AAuthPage()),
+              );
               // ...
             },
           ),
-
-
-
-
         ],
-
-
-
       ),
-      
-
     );
   }
 }
